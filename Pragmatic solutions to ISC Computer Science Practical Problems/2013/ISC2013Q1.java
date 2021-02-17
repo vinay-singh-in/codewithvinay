45751dbab41cedbe138842532148e0c418e9f259
vinay-singh-in <vinay.singh.ald@gmail.com>
2021-02-17 20:00:53 +0530

    Added solution for ISC 2013 Q1

--- Pragmatic solutions to ISC Computer Science Practical Problems/2013/ISC2013Q1.java
+++ Pragmatic solutions to ISC Computer Science Practical Problems/2013/ISC2013Q1.java
@@ -1,9 +1,56 @@
 import java.util.*;
 
+class ISBN {
+    private String isbn;
+    private int sum;
+
+    public ISBN(String isbn) {
+        this.isbn = isbn;
+        if (isbn.length() != 10)
+            throw new IllegalArgumentException();
+
+        for (int position = 0; position < 9; position++) {
+            if (!Character.isDigit(isbn.charAt(position))) {
+                throw new IllegalArgumentException();
+            }
+        }
+        char lastCharacter = isbn.charAt(9);
+        if (!(Character.isDigit(lastCharacter) || (lastCharacter == 'x' || lastCharacter == 'X'))) {
+            throw new IllegalArgumentException();
+        }
+        computeSum();
+    }
+
+    public void computeSum() {
+        int digit;
+        for (int position = 0; position < isbn.length(); position++) {
+            digit = isbn.charAt(position) - '0';
+            digit = (digit == 'x' || digit == 'X') ? 10 : digit;
+            sum += digit * (10 - position);
+        }
+    }
+
+    public int getSum() {
+        return sum;
+    }
+}
+
 class ISC2013Q1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        System.out.print("INPUT CODE:\t");
+        String isbn = in.nextLine();
+        try {
+            ISBN objISBN = new ISBN(isbn);
+            System.out.println("OUTPUT:\tSUM = " + objISBN.getSum());
+            if (objISBN.getSum() % 11 == 0) {
+                System.out.println("\tLEAVES NO REMAINDER – VALID ISBN CODE");
+            } else {
+                System.out.println("\tLEAVES REMAINDER – INVALID ISBN CODE");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("OUTPUT : INVALID INPUT");
+        }
         in.close();
     }
 }
