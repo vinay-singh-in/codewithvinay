cbd21a0391ebe07ab02f3a2966a67a7a5726cf91
vinay-singh-in <vinay.singh.ald@gmail.com>
2021-02-17 20:12:17 +0530

    Added comments for '0' adjustment

--- Pragmatic solutions to ISC Computer Science Practical Problems/2013/ISC2013Q1.java
+++ Pragmatic solutions to ISC Computer Science Practical Problems/2013/ISC2013Q1.java
@@ -15,20 +15,23 @@ class ISBN {
             }
         }
         char lastCharacter = isbn.charAt(9);
-        if(!(Character.isDigit(lastCharacter) || (lastCharacter=='x' || lastCharacter=='X'))){
+        if (!(Character.isDigit(lastCharacter) || (lastCharacter == 'x' || lastCharacter == 'X'))) {
             throw new IllegalArgumentException();
         }
         computeSum();
     }
-    public void computeSum(){
+
+    public void computeSum() {
         int digit;
-        for( int position=0; position<isbn.length();position++){
-            digit=isbn.charAt(position)-'0';
-            digit = (digit==40|| digit==72)?10:digit;
-            sum+=digit*(10-position);
+        for (int position = 0; position < isbn.length(); position++) {
+            // Note that the character from 0 to 9 has codes from 48 to 57
+            digit = isbn.charAt(position) - '0'; // '0' will result in subtraction of 48
+            digit = (digit == 40 || digit == 72) ? 10 : digit;
+            sum += digit * (10 - position);
         }
     }
-    public int getSum(){
+
+    public int getSum() {
         return sum;
     }
 }
@@ -40,10 +43,10 @@ class ISC2013Q1 {
         String isbn = in.nextLine();
         try {
             ISBN objISBN = new ISBN(isbn);
-            System.out.println("OUTPUT:\tSUM = "+objISBN.getSum());
-            if(objISBN.getSum()%11==0){
+            System.out.println("OUTPUT:\tSUM = " + objISBN.getSum());
+            if (objISBN.getSum() % 11 == 0) {
                 System.out.println("\tLEAVES NO REMAINDER – VALID ISBN CODE");
-            }else{
+            } else {
                 System.out.println("\tLEAVES REMAINDER – INVALID ISBN CODE");
             }
         } catch (IllegalArgumentException e) {
