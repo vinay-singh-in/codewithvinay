aeb90432f7f79bd8dabb7ec4d72060976e25e76f
vinay-singh-in <vinay.singh.ald@gmail.com>
2021-02-21 21:38:17 +0530

    Added solution template for ISC 2012 Q1

---
+++ Pragmatic solutions to ISC Computer Science Practical Problems/2012/ISC2012Q1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class ISC2012Q1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("INPUT:\t");
+        in.close();
+    }
+}
