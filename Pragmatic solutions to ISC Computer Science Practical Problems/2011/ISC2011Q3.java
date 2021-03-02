ac0ae7b9a0f88b76430c2c5da8da55fac7681b65
vinay-singh-in <vinay.singh.ald@gmail.com>
2021-03-02 06:41:39 +0530

    Added template for ISC 2011 Q3

---
+++ Pragmatic solutions to ISC Computer Science Practical Problems/2011/ISC2011Q3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class ISC2011Q3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("INPUT:\t");
+
+        in.close();
+    }
+}
