212ed94826ab6a4e858bb9aa0d959aac7d776962
vinay-singh-in <vinay.singh.ald@gmail.com>
2021-03-10 17:11:40 +0530

    Added ISC2009Q3.java

---
+++ Pragmatic solutions to ISC Computer Science Practical Problems/2009/ISC2009Q3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class ISC2009Q3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner( System.in);
+        System.out.print("INPUT:\t");
+        in.close();
+    }
+}
