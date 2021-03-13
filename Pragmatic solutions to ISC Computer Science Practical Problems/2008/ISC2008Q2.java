d220c78cdbbd2c18b20de5586920788d00acaa2f
vinay-singh-in <vinay.singh.ald@gmail.com>
2021-03-13 18:23:30 +0530

    Added solution template ISC 2008 Q2

---
+++ Pragmatic solutions to ISC Computer Science Practical Problems/2008/ISC2008Q2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ISC2008Q2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("INPUT:\t");
+
+        System.out.print("OUTPUT:\t");
+
+        in.close();
+    }
+}
