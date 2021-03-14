2a72b61a5f885b291e02edc7e95bbe33ea0a6e6b
vinay-singh-in <vinay.singh.ald@gmail.com>
2021-03-14 08:55:04 +0530

    Added question and template of ISC 2008 Q3

---
+++ Pragmatic solutions to ISC Computer Science Practical Problems/2008/ISC2008Q3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class ISC2008Q3{
+    public static void main(String[] args) {
+        Scanner in = new Scanner( System.in );
+        System.out.print("INPUT:\t");
+
+        System.out.print("OUTPUT:");
+
+        in.close();
+    }
+}
