9e6bb97189f049d1d4dfb2f750c2e0a560ed84bc
vinay-singh-in <vinay.singh.ald@gmail.com>
2021-03-17 18:13:59 +0530

    Added ISC 2007 Q1 question/template.

---
+++ Pragmatic solutions to ISC Computer Science Practical Problems/2007/ISC2007Q2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class ISC2007Q2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("INPUT:\t");
+
+        System.out.println("OUTPUT:");
+
+        in.close();
+    }
+}
