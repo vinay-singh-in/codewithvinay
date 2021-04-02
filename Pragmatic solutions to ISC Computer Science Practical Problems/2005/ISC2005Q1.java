012afa06b10d5524804ce5f46297e0f548fff334
vinay-singh-in <vinay.singh.ald@gmail.com>
2021-04-02 18:50:15 +0530

    Solution template for ISC 2005 Q1

---
+++ Pragmatic solutions to ISC Computer Science Practical Problems/2005/ISC2005Q1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class ISC2005Q1{
+    public static void main(String[] args) {
+        Scanner in = new Scanner( System.in );
+        System.out.print("INPUT:");
+
+        System.out.print("OUTPUT:");
+
+
+        in.close();
+    }
+}
