82387aad80e61ed614ac28be088aed61eeb2b169
vinay-singh-in <vinay.singh.ald@gmail.com>
2021-04-07 19:08:42 +0530

    Added question/template 2004 Q3

---
+++ Pragmatic solutions to ISC Computer Science Practical Problems/2004/ISC2004Q3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ISC2004Q3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner( System.in );
+        System.out.println("INPUT:");
+
+        System.out.println("OUTPUT:");
+
+
+        in.close();
+    }
+}
