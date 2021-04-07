536e85330eb54af00d9316024e2622e23fc3505e
vinay-singh-in <vinay.singh.ald@gmail.com>
2021-04-07 09:53:27 +0530

    Added ISC 2004 Q2

--- Pragmatic solutions to ISC Computer Science Practical Problems/2004/ISC2004Q2.java
+++ Pragmatic solutions to ISC Computer Science Practical Problems/2004/ISC2004Q2.java
@@ -1,11 +1,35 @@
 import java.util.*;
-
+class Message{
+    private String encodedMessage, decodedMessage;
+    public Message(String encodedMessage){
+        this.encodedMessage = encodedMessage;
+        decode();
+    }
+    private void decode(){
+        String temp="";
+        decodedMessage="";
+        int number;
+        for( int index=encodedMessage.length()-1; index>=0; index--){
+            temp+=encodedMessage.charAt(index);
+            number=Integer.parseInt(temp);
+            if(number==' ' || Character.isLetter(number)){
+                decodedMessage+=(char)number;
+                temp="";
+            }
+        }
+    }
+    public String toString(){
+        return decodedMessage;
+    }
+}
 public class ISC2004Q2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("INPUT:");
 
-        System.out.println("OUTPUT:");
+        System.out.println("INPUT:\nEncoded Message:");
+        String encodedMessage = in.next();
+        Message msg = new Message(encodedMessage);
+        System.out.println("OUTPUT:\n"+msg);
 
         in.close();
     }
